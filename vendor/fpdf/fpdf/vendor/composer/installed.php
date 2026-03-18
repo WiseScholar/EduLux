23eaaf22cd884771ea6c56ccd50daf156290b215
WiseScholar <ebenezertepe209@gmail.com>
2026-03-18 18:07:25 +0000

    vendor

---
+++ vendor/fpdf/fpdf/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fpdf/fpdf',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '82f5e728ff46ef25db582cabe01af228157c7a6b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fpdf/fpdf' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '82f5e728ff46ef25db582cabe01af228157c7a6b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

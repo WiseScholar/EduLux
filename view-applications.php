d42b058a5444ebe2f75f3e73cb8fffb44c8e60de
WiseScholar <ebenezertepe@gmail.com>
2026-04-20 03:49:36 +0000

    dine

--- view-applications.php
+++ view-applications.php
@@ -4,7 +4,7 @@ require_once ROOT_PATH . 'includes/functions.php';
 
 // Admin authentication required
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'instructor') {
-    header("Location: " . BASE_URL . "login.php");
+    header("Location: " . LOGIN_URL);
     exit;
 }
 
